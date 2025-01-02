81b93fec95fb54d149f3704e54e4fed3aba440c6
MaxSmile44 <afmaxim89@gmail.com>
2025-01-02 15:51:08 +0300

    Add Readme, deleted unnecessary files, optimized the code

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import argparse
 import collections
 import datetime
-from pprint import pprint
-
+import os
+from dotenv import load_dotenv
 import pandas
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -14,32 +15,44 @@ env = Environment(
 
 template = env.get_template('template.html')
 
-winery_age = datetime.date.today().year - datetime.date(year=1920, month=1, day=1).year
-year_name='лет' if (str(winery_age)[-2:] in list(map(str, list(range(11, 21))))
-                    or str(winery_age)[-1:] in list(map(str, list(range(5, 10))))
-                    or str(winery_age)[-1:] == '0') \
-    else 'года' if str(winery_age)[-1:] in list(map(str, list(range(2, 5)))) \
-    else 'год'
-excel_wines_df = pandas.read_excel(io='wine3.xlsx')
+winery_age = datetime.date.today().year - 1920
+year_name = 'лет'
+if (winery_age // 10) % 10 != 1:
+    if winery_age % 10 == 1:
+        year_name = 'год'
+    elif winery_age % 10 in (2, 3, 4):
+        year_name = 'года'
+
+load_dotenv()
+path = os.getenv('FILE_PATH')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--path', default=path, help='Path to excel file')
+args = parser.parse_args()
+
+excel_wines_df = pandas.read_excel(io=args.path)
 excel_wines_df = excel_wines_df.fillna('')
 wines = excel_wines_df.to_dict(orient='records')
 
-categories = excel_wines_df['Категория'].tolist()
 wine_categories = collections.defaultdict(list)
-for category in set(categories):
-    for wine in wines:
-        if category == wine['Категория']:
-            wine_categories[category].append(wine)
+for wine in wines:
+    wine_categories[wine['Категория']].append(wine)
 wine_categories = dict(wine_categories)
 
 rendered_page = template.render(
     winery_age=winery_age,
     year_name=year_name,
-    wine_categories = wine_categories
+    wine_categories=wine_categories
 )
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+def main():
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
