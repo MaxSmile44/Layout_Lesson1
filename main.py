010d396bcba76c46800b054728b3b87f61a12e8d
MaxSmile44 <afmaxim89@gmail.com>
2025-01-09 13:28:05 +0300

    Optimized functions and requirements.txt

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-DATE_ESTABLISHMENT = 1920
+ESTABLISHMENT_YEAR = 1920
 
 
 def get_age():
-    winery_age = datetime.date.today().year - DATE_ESTABLISHMENT
+    winery_age = datetime.date.today().year - ESTABLISHMENT_YEAR
     year_name = 'лет'
     if (winery_age // 10) % 10 != 1:
         if winery_age % 10 == 1:
@@ -21,14 +21,9 @@ def get_age():
             year_name = 'года'
     return f'{winery_age} {year_name}'
 
-def get_wines_info():
-    load_dotenv()
-    path = os.getenv('FILE_PATH')
-
-    parser = argparse.ArgumentParser(description='Print "python main.py -p {name of excel file with wine data}" '
-                                                 'or print "python main.py" '
-                                                 'if there is a file .env with the default file path')
-    parser.add_argument('-p', '--path', default=path, help='Path to excel file')
+def get_wines_info(path):
+    parser = argparse.ArgumentParser(description='Updates wine data on website from excel file')
+    parser.add_argument('-p', '--path', default=path, help='Print path to excel file')
     args = parser.parse_args()
 
     excel_wines_df = pandas.read_excel(io=args.path)
@@ -41,7 +36,7 @@ def get_wines_info():
     wine_categories = dict(wine_categories)
     return wine_categories
 
-def update_page():
+def update_page(age, wines_info):
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
@@ -50,13 +45,15 @@ def update_page():
     template = env.get_template('template.html')
 
     rendered_page = template.render(
-        winery_age=get_age(),
-        wine_categories=get_wines_info()
+        winery_age=age,
+        wine_categories=wines_info
     )
     return rendered_page
 
 def main():
-    rendered_page = update_page()
+    load_dotenv()
+    path = os.getenv('FILE_PATH')
+    rendered_page = update_page(get_age(), get_wines_info(path))
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
